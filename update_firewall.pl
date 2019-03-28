8e0bad06b38dc68102ca63bfe5bbe1c5cb673421
Arkashik Nbdy <arkashiks@gmail.com>
2019-03-28 21:47:20 +0100

    Updated

--- update_firewall.pl
+++ update_firewall.pl
@@ -16,26 +16,26 @@ my %vIPNames;
 while (my $vLine = <IPTABLES>)  {
     # only lines containing ip names (at least aaa.bbb)
     if ($vLine !~ /^\s*#/) {
-                if ($vLine =~ m/([a-zA-Z][^\s]+\.[^\s]+)/) {
-                        my $vIPName = $1;
-                        # ns lookup once only
-                        if (! exists $vIPNames{$vIPName}) {
-                                my $vIPAddress = "127.0.0.1";
-                                my @vHostInfo = gethostbyname($vIPName);
-                                if (scalar(@vHostInfo) == 0) {
-                                        print strftime("%F %T", localtime) , " gethostbyname: Can't resolve $vIPName: $!\n";
-                                        print strftime("%F %T", localtime) , " Aborting script execution.\n";
-                                        close IPTABLES;
-                                        exit;
-                                } else {
-                                        $vIPAddress = inet_ntoa(inet_aton($vIPName))
-                                                or die "", strftime("%F %T", localtime) , " ninet_ntoa: Can't resolve $vIPName: $!\n";
-                                        chomp $vIPAddress;
-                                        $vIPNames{$vIPName} = $vIPAddress;
-                                }
-                        }
+        if ($vLine =~ m/([a-zA-Z][^\s]+\.[^\s]+)/) {
+            my $vIPName = $1;
+            # ns lookup once only
+            if (! exists $vIPNames{$vIPName}) {
+                my $vIPAddress = "127.0.0.1";
+                my @vHostInfo = gethostbyname($vIPName);
+                if (scalar(@vHostInfo) == 0) {
+                    print strftime("%F %T", localtime) , " gethostbyname: Can't resolve $vIPName: $!\n";
+                    print strftime("%F %T", localtime) , " Aborting script execution.\n";
+                    close IPTABLES;
+                    exit;
+                } else {
+                    $vIPAddress = inet_ntoa(inet_aton($vIPName))
+                        or die "", strftime("%F %T", localtime) , " ninet_ntoa: Can't resolve $vIPName: $!\n";
+                    chomp $vIPAddress;
+                    $vIPNames{$vIPName} = $vIPAddress;
                 }
+            }
         }
+    }
 }
 close IPTABLES;
 
